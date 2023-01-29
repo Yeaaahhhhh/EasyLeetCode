05cb8c2f333afbe800293cd3428a26a29f6fc638
Dex <Dex@niubi.com>
2023-01-28 23:42:30 -0700

    next try

--- removeDupliArray.py
+++ removeDupliArray.py
@@ -1,9 +1,9 @@
-#Given an integer array nums sorted in non-decreasing order, remove the duplicates in-place such that each unique element appears only once. The relative order of the elements should be kept the same.
+#Giv333en an integer array nums sorted in non-decreasing order, remove the duplicates in-place such that each unique element appears only once. The relative order of the elements should be kept the same.
 #dagsdgsagsfghdgds
 #this line 1 Adgsdgagsagd
 #this line 2 xrcdsfsdgshshtvybunimoxrctvybun
 #easdgsgsgfgfg
-#Since it is impossible to change the length of the array in some languages, you must instead have the result be placed in the first part of the array nums. More formally, if there are k elements after removing the duplicates, then the first k elements of nums should hold the final result. It does not matter what you leave beyond the first k elements.
+#Since i333t is impossible to change the length of the array in some languages, you must instead have the result be placed in the first part of the array nums. More formally, if there are k elements after removing the duplicates, then the first k elements of nums should hold the final result. It does not matter what you leave beyond the first k elements.
 
 #Return k after placing the final result in the first k slots of nums.
 
